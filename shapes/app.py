8aa3d4c8e62c1812e77bd2914207b83631f160c5
James Kafader <jkafader@archive.org>
2020-09-25 21:24:38 -0700

    add song model, marshmallow schema

--- shapes/app.py
+++ shapes/app.py
@@ -10,3 +10,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 
 db = SQLAlchemy(app)
 mallow = Marshmallow(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer,primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    youtube_url = db.Column(db.String)
+    shape = db.Column(db.String)
+    
+    def __init__(self, title, youtube_url, shape):
+        self.title = title
+        self.youtube_url = youtube_url
+        self.shape = shape
+
+class SongSchema(mallow.Schema):
+    class Meta:
+        fields = ('id','title','youtube_url','shape')
+
+# Initialize schemas
+song_schema = SongSchema()
+songs_schema = SongSchema(many=True)
