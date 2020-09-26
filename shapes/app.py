bfa54774a68cad973c315ded0c6767f3d08104ea
James Kafader <jkafader@archive.org>
2020-09-26 19:51:56 +0000

    changes for validation (remove the 'id') field, correct the sqlite file path. Explicitly add call to create tables

--- shapes/app.py
+++ shapes/app.py
@@ -2,10 +2,11 @@
 from flask import Flask, request, jsonify 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import copy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///shapes.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///../www/shapes.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True 
 
 db = SQLAlchemy(app)
@@ -21,7 +22,7 @@ class Song(db.Model):
         self.title = title
         self.youtube_url = youtube_url
         self.shape = shape
-
+        
 class SongSchema(mallow.Schema):
     class Meta:
         fields = ('id','title','youtube_url','shape')
@@ -33,14 +34,17 @@ songs_schema = SongSchema(many=True)
 # Create a user
 @app.route('/song', methods=['POST'])
 def add_song():
+    validation_fields = copy.copy(SongSchema.Meta.fields)
+    validation_fields = list(validation_fields)
+    validation_fields.remove("id")
     # simple validation. compare each key in the schema with the keys we received in json post
-    for key in SongSchema.Meta.fields:
+    for key in validation_fields:
         if key not in request.json:
-            return 'JSON payload failed validation. Missing key: %s. Expected keys are: %s\n' % (key, SongSchema.Meta.fields), 400
+            return 'JSON payload failed validation. Missing key: %s. Expected keys are: %s\n' % (key, validation_fields), 400
     # simple validation. compare the length of the keys in the schema to the length of the
     # set of keys we received in json post.
-    if len(SongSchema.Meta.fields) != len(request.json):
-        return "JSON payload faled validation. Too many keys. (extraneous data?) Expected keys are: %s\n" % (SongSchema.Meta.fields,), 400
+    if len(validation_fields) != len(request.json):
+        return "JSON payload faled validation. Too many keys. (extraneous data?) Expected keys are: %s\n" % (validation_fields,), 400
 
     # use the "double star" operator to save some typing. Passes in the 
     # dictionary keys and values as arguments to the function (in this case, a constructor)
@@ -52,4 +56,6 @@ def add_song():
 # this tricky little weirdness is python boilerplate for "if this file is executed directly, do [action]"
 # where "action" here, is to call the "run" function on the flask "app" (in debug mode)
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
+
