c5d163160ce4f6dc5c5e081625d2b13c090c915b
James Kafader <jkafader@archive.org>
2020-09-27 22:39:44 +0000

    add examples of document types before tweaking schema to match older mongo version

--- shapes/app.py
+++ shapes/app.py
@@ -12,6 +12,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db = SQLAlchemy(app)
 mallow = Marshmallow(app)
 
+# Example of a Source document
+# {
+# 	"_id" : ObjectId("5eb0e81665bca35b26437f21"),
+# 	"parentEntity" : "Complex",
+# 	"parentStream" : "Best New Music This Week",
+# 	"instanceName" : "Brockhampton, Vince Staples, Lana Del Rey, and More",
+# 	"publicationDate" : "2019-08-23T00:00:00-07:00",
+# 	"location" : "https://www.complex.com/music/best-new-music-this-week-brockhampton-vince-staples-lana-del-rey"
+# }
+
+# Example of a Song document
+# {
+# 	"_id" : ObjectId("5eb0eb3d65bca35b26437f22"),
+# 	"captureDate" : "2020-05-04T21:25:11-07:00",
+# 	"captureSource" : ObjectId("5eb0e81665bca35b26437f21"),
+# 	"songName" : "St. Percy",
+# 	"artistName" : "Brockhampton",
+# 	"videoId" : "rp-I-YGg6Hs"
+# }
+
 class Song(db.Model):
     id = db.Column(db.Integer,primary_key=True, autoincrement=True)
     title = db.Column(db.String)
