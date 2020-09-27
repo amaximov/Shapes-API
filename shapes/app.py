7e1ef1cc4c71d4e3fe91f98b6935475bdea79ef0
James Kafader <jkafader@archive.org>
2020-09-27 22:08:23 +0000

    remove validation code, using built-in functionality of marshmallow schema instead

--- shapes/app.py
+++ shapes/app.py
@@ -23,7 +23,7 @@ class Song(db.Model):
         self.youtube_id = youtube_id
         self.shape = shape
         
-class SongSchema(mallow.Schema):
+class SongSchema(mallow.Schema):    
     class Meta:
         fields = ('id','title','youtube_id','shape')
 
@@ -34,20 +34,12 @@ songs_schema = SongSchema(many=True)
 # Create a user
 @app.route('/song', methods=['POST'])
 def add_song():
-    validation_fields = copy.copy(SongSchema.Meta.fields)
-    validation_fields = list(validation_fields)
-    validation_fields.remove("id")
-    # simple validation. compare each key in the schema with the keys we received in json post
-    for key in validation_fields:
-        if key not in request.json:
-            return 'JSON payload failed validation. Missing key: %s. Expected keys are: %s\n' % (key, validation_fields), 400
-    # simple validation. compare the length of the keys in the schema to the length of the
-    # set of keys we received in json post.
-    if len(validation_fields) != len(request.json):
-        return "JSON payload faled validation. Too many keys. (extraneous data?) Expected keys are: %s\n" % (validation_fields,), 400
-
     # use the "double star" operator to save some typing. Passes in the 
     # dictionary keys and values as arguments to the function (in this case, a constructor)
+    try:
+        song_schema.load(request.json)
+    except mallow.ValidationError as err:
+        return err.messages, 400
     new_song = Song(**request.json)
     db.session.add(new_song)
     db.session.commit()
@@ -56,7 +48,28 @@ def add_song():
 @app.route('/song', methods=['GET'])
 def get_songs():
     songs = Song.query.all()
-    return jsonify(songs_schema.dump(songs))                  #or you can write single line: jsonify(users_schema.dump(users))
+    return jsonify(songs_schema.dump(songs))
+
+@app.route("/song/<int:id>", methods=['PUT'])
+def update_song(song_id):
+    song = Song.query.get(song_id)
+    try:
+        updated_song = song_schema.load(request.json, instance=song)
+    except mallow.ValidationError as err:
+        return err.messages, 400
+    db.session.commit()
+    return song_schema.jsonify(song)
+
+@app.route("/song/<int:id>", methods=['DELETE'])
+def delete_song(song_id):
+    song = Song.query.get(song_id)
+    db.session.delete(song)
+    db.session.commit()
+    return song_schema.jsonify(song)
+
+
+    
+
 
 # this tricky little weirdness is python boilerplate for "if this file is executed directly, do [action]"
 # where "action" here, is to call the "run" function on the flask "app" (in debug mode)
