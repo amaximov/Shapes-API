814f04c975a1824e82aa25c69510ec1fc39c425c
James Kafader <jkafader@archive.org>
2020-09-29 21:54:57 +0000

    updated swagger.json and added docstrings

--- shapes/app.py
+++ shapes/app.py
@@ -2,10 +2,14 @@
 from flask import Flask, request, jsonify 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.flask import FlaskPlugin
 from marshmallow.exceptions import ValidationError
 from sqlalchemy.orm import scoped_session, sessionmaker
 from shapes.models import db, Source, Artist, Song, Shape, SourceSchema, ArtistSchema, SongSchema, ShapeSchema
+from apispec import APISpec
 import copy
+import json
 
 app = Flask(__name__)
 
@@ -17,6 +21,22 @@ app.app_context().push()
 db.create_all()
 mallow = Marshmallow(app)
 
+# Create spec
+spec = APISpec(
+    title='Shapes API',
+    version='1.0.0',
+    info=dict(
+        description='An API that describes some datatypes for support of the Shapes system'
+    ),
+    openapi_version="3.0.2",
+    plugins=[
+        FlaskPlugin(),
+        MarshmallowPlugin()
+    ]
+)
+
+spec.components.schema("Song", schema=SongSchema)
+
 # Initialize schemas
 song_schema = SongSchema()
 songs_schema = SongSchema(many=True)
@@ -24,6 +44,20 @@ songs_schema = SongSchema(many=True)
 # Create a song
 @app.route('/song', methods=['POST'])
 def add_song():
+    """Add Song view.
+    ---
+    post:
+      summary: Create Song
+      description: Post a single Song.
+      responses:
+        200:
+          description: Song creation succeeded
+          content:
+            application/json:
+              schema: SongSchema
+        400:
+          description: Creation failed. More details in error message.              
+    """
     try:
         new_song = song_schema.load(request.json, session=db.session)
     except ValidationError as err:
@@ -34,11 +68,43 @@ def add_song():
 
 @app.route('/song', methods=['GET'])
 def get_songs():
+    """Get Songs.
+    ---
+    get:
+      summary: List Songs
+      description: Lists all songs
+      responses:
+        200:
+          description: Song listing succeeded
+          content:
+            application/json:
+              schema: SongSchema
+    """
     songs = Song.query.all()
     return jsonify(songs_schema.dump(songs))
 
 @app.route("/song/<int:id>", methods=['PUT'])
 def update_song(song_id):
+    """Update Song
+    ---
+    put:
+      summary: Update Song
+      description: Update Song identified by <id>
+      parameters:
+        - name: song_id
+          in: path
+          description: Song ID
+          type: integer
+          required: true
+      responses:
+        200:
+          description: Song update succeeded
+          content:
+            application/json:
+              schema: SongSchema
+        400:
+          description: Update failed. More details in error message.              
+    """
     song = Song.query.get(song_id)
     try:
         updated_song = song_schema.load(request.json, instance=song)
@@ -49,11 +115,37 @@ def update_song(song_id):
 
 @app.route("/song/<int:id>", methods=['DELETE'])
 def delete_song(song_id):
+    """Delete Song
+    ---
+    delete:
+      summary: Delete Song
+      description: Delete Song identified by <id>
+      parameters:
+        - name: song_id
+          in: path
+          description: Song ID
+          type: integer
+          required: true
+      responses:
+        200:
+          description: Song delete succeeded
+        400:
+          description: Deletion failed. More details in error message.              
+    """
     song = Song.query.get(song_id)
     db.session.delete(song)
     db.session.commit()
     return jsonify(song_schema.dumps(song))
 
+with app.test_request_context():
+    spec.path(view=add_song)
+    spec.path(view=delete_song)
+    spec.path(view=update_song)
+    spec.path(view=get_songs)
+
+with open('www/swagger.json', 'w') as f:
+    json.dump(spec.to_dict(), f)
+    
 # this tricky little weirdness is python boilerplate for "if this file is executed directly, do [action]"
 # where "action" here, is to call the "run" function on the flask "app" (in debug mode)
 if __name__ == '__main__':
